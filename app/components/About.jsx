06fb98ea8bda2ae9d6e3f10c378ff0d3d8563994
Haris Sucipto <harisssucipto@gmail.com>
2017-04-28 17:05:36 +0700

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,22 @@ var React = require('react');
 // Stateless dont maintaint any state
 var About = (props) => {
   return (
-    <h3>About</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application buid on React</p>
+      <p>Here a some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I Used Open Weather map
+            to Search for weather data by city name.
+        </li>
+      </ul>
+
+    </div>
+
   )
 }
 
