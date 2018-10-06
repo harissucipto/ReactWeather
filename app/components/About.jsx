bdbfafaccbba20dab0534185dd728753cd57526a
anan474 <anan474@myself.com>
2018-10-06 23:08:21 +0700

    rapikan dan hapus tak penting

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,7 @@
 var React = require('react');
 
-
 // Stateless dont maintaint any state
-var About = (props) => {
+var About = props => {
   return (
     <div>
       <h1 className="text-center page-title">About</h1>
@@ -10,17 +9,16 @@ var About = (props) => {
       <p>Here a some of the tools I used:</p>
       <ul>
         <li>
-          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+          <a href="https://facebook.github.io/react">React</a> - This was the
+          Javascript framework used.
         </li>
         <li>
-          <a href="http://openweathermap.org">Open Weather Map</a> - I Used Open Weather map
-            to Search for weather data by city name.
+          <a href="http://openweathermap.org">Open Weather Map</a> - I Used Open
+          Weather map to Search for weather data by city name.
         </li>
       </ul>
-
     </div>
-
-  )
-}
+  );
+};
 
 module.exports = About;
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -19,23 +19,27 @@ var Weather = React.createClass({
       temp: undefined
     });
 
-    openWeatherMap.getTemp(location).then(function(temp) {
-      that.setState({
-        location: location,
-        temp: temp,
-        isLoading: false
-      });
-    }, function(e) {
-      that.setState({
-        isLoading: false,
-        errorMessage: e.message
-      });
-    });
+    // ambil data temperatur berdasarkan lokasi
+    openWeatherMap.getTemp(location).then(
+      function(temp) {
+        that.setState({
+          location: location,
+          temp: temp,
+          isLoading: false
+        });
+      },
+      function(e) {
+        that.setState({
+          isLoading: false,
+          errorMessage: e.message
+        });
+      }
+    );
   },
   componentDidMount: function() {
     var location = this.props.location.query.location;
 
-    if (location && location.length > 0){
+    if (location && location.length > 0) {
       this.handleSearch(location);
       window.location.hash = '#/';
     }
@@ -43,34 +47,32 @@ var Weather = React.createClass({
   componentWillReceiveProps: function(newProps) {
     var location = newProps.location.query.location;
 
-    if (location && location.length > 0){
+    if (location && location.length > 0) {
       this.handleSearch(location);
       window.location.hash = '#/';
     }
   },
   render: function() {
-    var {isLoading, location, temp, errorMessage} = this.state;
+    var { isLoading, location, temp, errorMessage } = this.state;
 
     function renderMessage() {
       if (isLoading) {
         return <h3 className="text-center">Fetching weather...</h3>;
-      } else if(temp && location) {
-        return <WeatherMessage temp={temp} location={location}/>;
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location} />;
       }
     }
 
     function renderError() {
-      if (typeof errorMessage == 'string'){
-        return (
-          <ErrorModal message={errorMessage}/>
-        )
+      if (typeof errorMessage == 'string') {
+        return <ErrorModal message={errorMessage} />;
       }
     }
 
     return (
       <div>
         <h1 className="text-center page-title">Get Weather</h1>
-        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherForm onSearch={this.handleSearch} />
         {renderMessage()}
         {renderError()}
       </div>
@@ -78,4 +80,4 @@ var Weather = React.createClass({
   }
 });
 
-module.exports= Weather;
+module.exports = Weather;
